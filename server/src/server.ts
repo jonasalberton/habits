9c15102cfe235fabd721efde130c846ba321d08c
jonasalberton <jonasceolin@gmail.com>
2023-01-18 14:35:17 -0300

    create POST habits GET day

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import Fastify from "fastify"
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
-
-const prisma = new PrismaClient()
+import { appRoutes } from "./routes"
 
 const PORT = 3333
 const app = Fastify()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
 
 app.listen({ port: PORT }).then(() => console.log(`Server running at http://localhost${PORT}`)
 )
