00db8ecbc36ae65421dffc150507e440d914e402
jonasalberton <jonasceolin@gmail.com>
2023-01-19 16:55:46 -0300

    wip frontt-end

--- front/src/components/SummaryTable.tsx
+++ front/src/components/SummaryTable.tsx
@@ -22,7 +22,7 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => <HabitDay key={date.toString()} />)}
+        {summaryDates.map((date) => <HabitDay key={date.toString()} amount={5} completed={4}/>)}
         {ammountOfDaysToFill > 0 && Array.from({ length: ammountOfDaysToFill}).map((_, index) => ( <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-50 cursor-not-allowed"></div>))}
       </div>
     </div>
